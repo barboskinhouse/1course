29a81c1d2783e303d3b70b5ef23b8dbeb44adaa3
koooffa <91970205+koooffa@users.noreply.github.com>
2022-05-17 22:16:37 +0300

    Add files via upload

---
+++ Project_SubList_py.py
@@ -0,0 +1,70 @@
+# Изменение изначального списка [0, ..., n]
+# Первые k эл-тов преобразует в следующую комбинацию
+def NextList(Lst, n, k):
+    for i in range(k-1, 0-1, -1):
+        if(Lst[i] < n - k + i):
+            Lst[i] += 1
+            for j in range(i+1, k):
+                Lst[j] = Lst[j-1] + 1
+
+            return True
+
+    return False
+                
+
+# Создаёт промежуточный вложенный список
+def Add(L, k):
+    buf = []
+    for i in range(k):
+        buf.append(L[i])
+
+    return buf
+
+# Возвращает список с вложенными списками из комбинаций индексов
+# всех возможных сочетаний из n по k,
+# где k принимает значения от 0 до n
+def index(n):
+    L = [[]]   
+
+    for i in range(1, n+1):
+        L0 = [i for i in range(n)]
+        k = i
+        buf = Add(L0, k)
+        L.append(buf)
+        print(L0)
+        if(n > k):
+            while(NextList(L0, n, k)):
+                buf = Add(L0, k)
+                L.append(buf)
+                print(L0)
+                print(L)
+
+    return L
+
+# Составляет итоговый список сочетаний введённых эл-тов
+def combination(L):
+    comb = []
+    for elem in L:
+        buf = []
+        for i in range(len(elem)):
+            buf.append(s[elem[i]])
+        if(not(buf in comb)):
+            comb.append(buf)
+
+    return comb
+
+
+s = input('Введите эл-ты: ')
+s = s.split()   
+n = len(s)
+
+
+
+Idx = index(n)
+comb = combination(Idx)  
+
+print(comb)
+
+
+
+    
---
+++ first.py
@@ -0,0 +1,12 @@
+'''
+Разделение списка на подсписки
+'''
+
+st = input().split()
+lst=[[]]
+for i in range(1, len(st)+1):
+    for j in range(len(st)-i+1):
+        lst +=[st[j:j+i]]
+one=list(st[0]+st[len(st)-1])
+lst.append(one)
+print(lst)
